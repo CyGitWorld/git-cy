a316d8d003357a36104a14c27a8c353e0f1efecc
euijinkk <gytks4@naver.com>
2024-05-05 16:09:09 +0900

    feat: 상수 제거

--- client/web/src/app/oauth/GithubLoginButton.tsx
+++ client/web/src/app/oauth/GithubLoginButton.tsx
@@ -3,15 +3,13 @@
 import { useRouter } from "next/navigation";
 import queryString from "query-string";
 
-import { GITHUB_AUTHORIZE_SERVER_URL } from "./constant";
-
 export function GithubLoginButton() {
   const router = useRouter();
   return (
     <button
       onClick={() => {
         const githubAuthUri = queryString.stringifyUrl({
-          url: GITHUB_AUTHORIZE_SERVER_URL,
+          url: "https://github.com/login/oauth/authorize",
           query: {
             client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
             redirect_uri: `${window.location.origin}/oauth/redirect`,
--- client/web/src/app/oauth/constant.ts
+++
@@ -1,3 +0,0 @@
-const GITHUB_OAUTH_URL = "https://github.com/login/oauth";
-export const GITHUB_AUTHORIZE_SERVER_URL = `${GITHUB_OAUTH_URL}/authorize`;
-export const GITHUB_ACCESS_TOKEN_SERVER_URL = `${GITHUB_OAUTH_URL}/access_token`;
