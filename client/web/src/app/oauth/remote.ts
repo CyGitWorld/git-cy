451bc2a0dc1c667fbaa70ea28e5dd9c5a9368661
euijinkk <gytks4@naver.com>
2024-05-05 16:11:42 +0900

    chore

--- client/web/src/app/oauth/remote.ts
+++
@@ -1,8 +0,0 @@
-type LogingByGithubRequest = { code: string };
-type LogingByGithubResponse = { userName: string };
-export function loginByGithub({ code }: LogingByGithubRequest) {
-  console.log("code", code);
-  return new Promise<LogingByGithubResponse>((res) =>
-    res({ userName: "김의진" })
-  );
-}
