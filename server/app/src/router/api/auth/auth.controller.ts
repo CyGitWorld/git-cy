dc48208f40f7b0f8422b2ef06cf85826d967d8fc
euijinkk <gytks4@naver.com>
2024-05-05 16:06:24 +0900

    feat: 401 exception

--- server/app/src/router/api/auth/auth.controller.ts
+++ server/app/src/router/api/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import queryString from "query-string";
 import { z } from "zod";
 import { AuthService } from "./auth.service";
+import { HTTPException } from "hono/http-exception";
 
 type Bindings = {
   CLIENT_ID: string;
@@ -19,17 +19,24 @@ export const createAuthServer = (authService: AuthService) => {
       })
     ),
     async (ctx) => {
-      const { code } = ctx.req.valid("json");
+      try {
+        const { code } = ctx.req.valid("json");
 
-      const accessToken = await authService.getGithubAccessToken({
-        clientId: ctx.env.CLIENT_ID,
-        clientSecret: ctx.env.CLIENT_SECRET,
-        code,
-      });
+        const accessToken = await authService.getGithubAccessToken({
+          clientId: ctx.env.CLIENT_ID,
+          clientSecret: ctx.env.CLIENT_SECRET,
+          code,
+        });
 
-      const { name } = await authService.getGithbuUserInfo({ accessToken });
+        const { name } = await authService.getGithbuUserInfo({ accessToken });
 
-      return ctx.json({ success: true, data: { accessToken, name } }, 200);
+        return ctx.json({ success: true, data: { accessToken, name } }, 200);
+      } catch (e) {
+        throw new HTTPException(401, {
+          message: "Github 로그인에 실패했어요",
+          cause: e,
+        });
+      }
     }
   );
 };
