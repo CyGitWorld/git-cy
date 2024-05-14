edfd54dfb9e3edebb8b1b173abd786eda1523e57
김의진 <gytks4@naver.com>
2024-05-14 10:49:45 +0900

    chore: kysely, d1 세팅 (#10)
    * chore: kysely, d1 세팅
    
    * feat: console 제거
    
    * chore: 주석 제거

--- server/app/src/router/api/test/index.ts
+++ server/app/src/router/api/test/index.ts
@@ -1,10 +1,30 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { Kysely } from "kysely";
+import { D1Dialect } from "kysely-d1";
 import { z } from "zod";
 
+type Bindings = {
+  DEV_DB: D1Database;
+};
+
+type DataBase = {
+  Customers: { CustomerId: number; CompanyName: string; ContactName: string };
+};
+
 export const creaetTestRouter = () => {
-  return new Hono()
-    .get("/v1/hono-test-get", (c) => {
+  return new Hono<{ Bindings: Bindings }>()
+    .get("/v1/hono-test-get", async (c) => {
+      const db = new Kysely<DataBase>({
+        dialect: new D1Dialect({ database: c.env.DEV_DB }),
+      });
+
+      const result = await db
+        .selectFrom("Customers")
+        .selectAll()
+        .where("CustomerId", "=", 1)
+        .executeTakeFirst();
+
       return c.json({ result: "test success 1" }, 200);
     })
     .post(
