4c058d890d087802c767a8057dbf73ccbc2909a7
gytks4 <gytks4@naver.com>
2024-05-25 20:05:52 +0900

    chore: delete unused import

--- server/app/src/router/api/auth/auth.controller.ts
+++ server/app/src/router/api/auth/auth.controller.ts
@@ -4,8 +4,7 @@ import { z } from "zod";
 import { AuthService } from "./auth.service";
 import { HTTPException } from "hono/http-exception";
 import { Env } from "../../../worker";
-import { decode, jwt, sign, verify } from "hono/jwt";
-import { bearerAuth } from "hono/bearer-auth";
+import { jwt, verify } from "hono/jwt";
 
 export const createAuthController = ({
   service: authService,
