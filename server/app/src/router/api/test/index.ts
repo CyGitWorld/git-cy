8249796bd507211e21983f3966aba377d2486a9f
gytks4 <gytks4@naver.com>
2024-05-25 22:23:43 +0900

    chore

--- server/app/src/router/api/test/index.ts
+++ server/app/src/router/api/test/index.ts
@@ -1,44 +1,20 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Kysely } from "kysely";
-import { D1Dialect } from "kysely-d1";
 import { z } from "zod";
 
-type Bindings = {
-  DEV_DB: D1Database;
-};
-
-type DataBase = {
-  Customers: { CustomerId: number; CompanyName: string; ContactName: string };
-};
-
 export const creaetTestRouter = () => {
-  return new Hono<{ Bindings: Bindings }>()
-    .get("/v1/hono-test-get", async (c) => {
-      const db = new Kysely<DataBase>({
-        dialect: new D1Dialect({ database: c.env.DEV_DB }),
-      });
+  return new Hono().post(
+    "/v1/hono-test-post",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+      })
+    ),
+    (c) => {
+      const { name } = c.req.valid("json");
 
-      const result = await db
-        .selectFrom("Customers")
-        .selectAll()
-        .where("CustomerId", "=", 1)
-        .executeTakeFirst();
-
-      return c.json({ result: "test success 1" }, 200);
-    })
-    .post(
-      "/v1/hono-test-post",
-      zValidator(
-        "json",
-        z.object({
-          name: z.string(),
-        })
-      ),
-      (c) => {
-        const { name } = c.req.valid("json");
-
-        return c.json({ result: `Hello ${name}` });
-      }
-    );
+      return c.json({ result: `Hello ${name}` });
+    }
+  );
 };
