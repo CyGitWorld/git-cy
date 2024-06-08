cc8ecb0c12c15dfb33c16ac8aeffa9fcf9c6ba2f
joohaem-lee <joohaem.lee@toss.im>
2024-06-08 18:53:45 +0900

    feat: 방명록 페이지 ui 구현

--- client/web/src/app/minihome/[user-name]/guestbook/page.tsx
+++ client/web/src/app/minihome/[user-name]/guestbook/page.tsx
@@ -1,3 +1,21 @@
+import { Divider } from "@/components/divider";
+
+import { UserInput } from "./components/user-input";
+import { guestbook, h1 } from "./page.css";
+
 export default function Guestbook() {
-  return <div>hello</div>;
+  return (
+    <>
+      <h1 className={h1}>Guestbook</h1>
+      <section>
+        <UserInput />
+        <article className={guestbook}>mapping</article>
+        <Divider />
+        <article className={guestbook}>mapping</article>
+        <Divider />
+        <article className={guestbook}>mapping</article>
+        <Divider />
+      </section>
+    </>
+  );
 }
