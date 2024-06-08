ea62711e370c73676b54e309001d48d9a0b32e3c
joohaem-lee <joohaem.lee@toss.im>
2024-06-08 19:59:33 +0900

    test: communication

--- client/web/src/app/minihome/[user-name]/guestbook/page.css.ts
+++ client/web/src/app/minihome/[user-name]/guestbook/page.css.ts
@@ -3,11 +3,3 @@ import { style } from "@vanilla-extract/css";
 export const h1 = style({
   marginBottom: "48px",
 });
-
-export const guestbook = style({
-  display: "flex",
-  flexDirection: "column",
-  gap: "20px",
-  paddingTop: "32px",
-  paddingBottom: "32px",
-});
--- client/web/src/app/minihome/[user-name]/guestbook/page.tsx
+++ client/web/src/app/minihome/[user-name]/guestbook/page.tsx
@@ -1,20 +1,12 @@
-import { Divider } from "@/components/divider";
-
-import { UserInput } from "./components/user-input";
-import { guestbook, h1 } from "./page.css";
+import { GuestbookContent } from "./components/guestbook-content";
+import { h1 } from "./page.css";
 
 export default function Guestbook() {
   return (
     <>
       <h1 className={h1}>Guestbook</h1>
       <section>
-        <UserInput />
-        <article className={guestbook}>mapping</article>
-        <Divider />
-        <article className={guestbook}>mapping</article>
-        <Divider />
-        <article className={guestbook}>mapping</article>
-        <Divider />
+        <GuestbookContent />
       </section>
     </>
   );
