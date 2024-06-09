9dfd964f6d37f75f098ac3238022c225fc72b9dd
gytks4 <gytks4@naver.com>
2024-06-10 00:11:12 +0900

    feat: sql 파일 통합

--- server/app/src/router/api/guestbook/guestbook.schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Guestbooks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,    
-    minihomeId INTEGER UNIQUE,
-    createdAt TEXT,
-    updatedAt TEXT,
-    FOREIGN KEY (minihomeId) REFERENCES Minihomes(id)
-);
--- server/app/src/router/api/minihome/minihome.schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Minihomes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,    
-    userId INTEGER UNIQUE,
-    createdAt TEXT,
-    updatedAt TEXT,
-    FOREIGN KEY (userId) REFERENCES Users(id)
-);
