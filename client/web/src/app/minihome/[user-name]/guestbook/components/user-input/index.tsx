7ca2925365315b1f88f84b1a8b75752263451849
joohaem-lee <joohaem.lee@toss.im>
2024-06-15 22:27:05 +0900

    chore: stash

--- client/web/src/app/minihome/[user-name]/guestbook/components/user-input/index.tsx
+++ client/web/src/app/minihome/[user-name]/guestbook/components/user-input/index.tsx
@@ -1,14 +1,36 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
 import { Button } from "@/components/button";
 import { TextInput } from "@/components/text-input";
 
 import { wrapper } from "./index.css";
 
 export const UserInput = () => {
+  const [comment, setComment] = useState("");
+  // const { mutate } = useMutation({
+  //   mutationFn: ({ code }: GuestbookType) =>
+  //     requestApiJson((api) =>
+  //       api.users.login.$post({
+  //         json: {
+  //           code,
+  //         },
+  //       })
+  //     ),
+  // });
+
   return (
     <div className={wrapper}>
-      <TextInput multiline fullWidth />
+      <TextInput
+        multiline
+        fullWidth
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
       <Button>OK</Button>
     </div>
   );
