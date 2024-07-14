5e87c6d664837c0c53014c95a7ba0f5591dd3fd0
Tekiter <tekiter8@gmail.com>
2024-07-14 21:08:15 +0900

    feat: jsx 적용

--- server/app/src/router/widget/guestbook/index.tsx
+++ server/app/src/router/widget/guestbook/index.tsx
@@ -1,5 +1,6 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import {} from "hono/jsx";
 import { z } from "zod";
 
 export const createGuestbookRoute = () => {
@@ -20,7 +21,21 @@ export const createGuestbookRoute = () => {
         <text x="0" y="30" font-family="'sans-serif'" font-size="20" fill="#000000">${githubUserName}</text>
       </svg>`;
 
-      const response = new Response(data, {
+      const svg = await (
+        <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
+          <text
+            x="0"
+            y="30"
+            fontFamily="'sans-serif'"
+            fontSize="20"
+            fill="#000000"
+          >
+            {githubUserName}
+          </text>
+        </svg>
+      );
+
+      const response = new Response(svg, {
         headers: {
           "Content-Type": "image/svg+xml",
           "Access-Control-Allow-Origin": "*",
