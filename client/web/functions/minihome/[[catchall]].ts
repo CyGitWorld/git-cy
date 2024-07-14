a509f6a8e37fba66a184ecfa6862ca2d24630c42
Tekiter <tekiter8@gmail.com>
2024-07-14 18:39:31 +0900

    feat: 미니홈 path 추가

--- client/web/functions/minihome/[[catchall]].ts
+++ client/web/functions/minihome/[[catchall]].ts
@@ -6,14 +6,11 @@ export const onRequest: PagesFunction = async (context) => {
 
   const catchall = params.catchall;
 
-  console.log(JSON.stringify(params));
-  console.log("catchall", catchall);
-
   if (typeof catchall === "string") {
     return next("/404");
   }
 
-  const convertedPath = `/${["__dynamic__", ...catchall.slice(1)].join("/")}`;
+  const convertedPath = `/minihome/${["__dynamic__", ...catchall.slice(1)].join("/")}`;
 
   console.log("converted", convertedPath);
 
