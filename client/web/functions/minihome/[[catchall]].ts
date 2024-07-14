07e57476bb44967ac6f133f6589c7160fabf48ee
Tekiter <tekiter8@gmail.com>
2024-07-14 18:15:23 +0900

    feat: 로그 추가

--- client/web/functions/minihome/[[catchall]].ts
+++ client/web/functions/minihome/[[catchall]].ts
@@ -1,12 +1,13 @@
 /* eslint-disable no-undef */
 
-// /minihome/__dynamic__/* 주소로 직접 접근 시 /minihome/__dynamic__/*.html 로 보내기
+// /minihome/[username]/* 주소로 직접 접근 시 /minihome/__dynamic__/*.html 로 보내기
 export const onRequest: PagesFunction = async (context) => {
   const { next, params } = context;
 
   const catchall = params.catchall;
 
-  console.log(catchall);
+  console.log(JSON.stringify(params));
+  console.log("catchall", catchall);
 
   if (typeof catchall === "string") {
     return next("/404");
